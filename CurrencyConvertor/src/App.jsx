9e34193218f94d320f5f70f40bb00944123b2aa3
vinayka2601 <vinaykaawasthi26@gmail.com>
2024-07-31 20:46:55 +0530

    newCommit

--- CurrencyConvertor/src/App.jsx
+++ CurrencyConvertor/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
-import {InputBox} from './components'
+import {InputBox} from './Components'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
 
 
 function App() {
 
-  const [amount, setAmount] = useState(0)
+  const [amount, setAmount] = useState(1)
   const [from, setFrom] = useState("usd")
   const [to, setTo] = useState("inr")
   const [convertedAmount, setConvertedAmount] = useState(0)
@@ -37,7 +37,7 @@ function App() {
                 <form
                     onSubmit={(e) => {
                         e.preventDefault();
-                        convert()
+                        convert();
                        
                     }}
                 >
@@ -54,20 +54,22 @@ function App() {
                     <div className="relative w-full h-0.5">
                         <button
                             type="button"
-                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
+                            className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5 hover:bg-blue-700"
                             onClick={swap}
                         >
                             swap
                         </button>
                     </div>
-                    <div className="w-full mt-1 mb-4">
+                    <div className="w-full mt-1 mb-4 ">
                         <InputBox
                             label="To"
                             amount={convertedAmount}
                             currencyOptions={options}
                             onCurrencyChange={(currency) => setTo(currency)}
-                            selectCurrency={from}
+                            selectCurrency={to}
                             amountDisable
+
+                            
                         />
                     </div>
                     <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
--- CurrencyConvertor/src/Components/InputBox.jsx
+++ CurrencyConvertor/src/Components/InputBox.jsx
@@ -33,11 +33,13 @@ function InputBox({
                 <p className="text-black/40 mb-2 w-full">Currency Type</p>
                 <select
                     className="rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none"
+                
+                    
                     value={selectCurrency}
                     onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
                     disabled={currencyDisable}
                 >
-                    
+            
                         {currencyOptions.map((currency) => (
                             <option key={currency} value={currency}>
                             {currency}
